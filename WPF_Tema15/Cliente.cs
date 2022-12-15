7e73489af00747d171c09f593d33eb5f2575b5d2
20211011110003 <20211011110003@CNAT407159A>
2022-12-15 11:35:46 -0300

    Adicionar arquivos de projeto.

---
+++ WPF_Tema15/Cliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Tema15
+{
+    class Cliente
+    {
+        private string nome, cpf;
+        private Conta[] contas;
+
+        public void Inserir(Conta[] c)
+        {
+
+        }
+        public Conta[] Listar()
+        {
+
+        }
+        public double SaldoTotal()
+        {
+
+        }
+        public string ToString()
+        {
+
+        }
+    }
+}
---
+++ WPF_Tema15/Conta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Tema15
+{
+    class Conta
+    {
+        private string numero, agencia;
+        private double saldo;
+        public override string ToString();
+    }
+}
